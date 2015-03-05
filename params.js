ca3a250ffe69470e6ca963074b09f63260372c2a
Eric <eric.basley@redpelicans.com>
2015-03-05 11:45:45 +0100

    add params.js template

---
+++ params.js
@@ -0,0 +1,15 @@
+module.exports = {
+  app: {
+    name: 'app',
+    repo: 'https://github.com/redpelicans/dockerized-app.git',
+  },
+  docker: {
+    name: 'redpelicans',
+    auth:{
+      username: '',
+      password: '',
+      email: '',
+      serveraddress: "https://index.docker.io/v1/"
+    }
+  }
+}
